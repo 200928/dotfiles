895ff014893370f951d43f7eb64f92a4f43e741f
minghuifan <755400668@qq.com>
2024-05-12 15:26:00 +0800

    hyprland update

---
+++ hypr/scripts/randomWallpaper.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+DIR=$HOME/wallpaper/
+PICS=($(find ${DIR} -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" \)))
+RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
+
+change_swaybg(){
+  pkill swww
+  pkill swaybg
+  swaybg -m fill -i ${RANDOMPICS}
+}
+
+change_swww(){
+  pkill swaybg
+  swww query || swww init
+  swww img ${RANDOMPICS} --transition-fps 30 --transition-type any --transition-duration 3
+}
+
+change_current(){
+  if [ "$(cat ~/.wallpaper_app)" = "swaybg" ]; then
+    change_swaybg
+  else
+    change_swww
+  fi
+}
+
+switch(){
+  if pidof swaybg >/dev/null; then
+    change_swww
+    echo "swww" > ~/.wallpaper_app
+  else
+    change_swaybg
+    echo "swaybg" > ~/.wallpaper_app
+  fi
+}
+
+case "$1" in
+	"swaybg")
+		change_swaybg
+		;;
+	"swww")
+		change_swww
+		;;
+  "s")
+		switch
+		;;
+	*)
+		change_current
+		;;
+esac
